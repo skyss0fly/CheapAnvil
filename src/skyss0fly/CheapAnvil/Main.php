5e45bc63a3c116ce5d3b89ce64297decfc9938cb
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-08-28 19:43:21 +1000

    Update Main.php

--- src/skyss0fly/CheapAnvil/Main.php
+++ src/skyss0fly/CheapAnvil/Main.php
@@ -1,16 +1,37 @@
 <?php
 namespace skyss0fly\CheapAnvil;
 
-use pocketmine\item\enchantment\EnchantmentOption;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\inventory\AnvilInventory;
+use pocketmine\Player;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener {
 
-public function onEnable(EnchantmentOption $enc):void {
-$enc->requiredXpLevel = 0;
-  $this->getLogger()->info("CheapAnvil has set Anvil Price to 0 xp");
-  
-  return true;
-  
-}
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("CheapAnvil has been enabled and set Anvil Price to 0 xp");
+    }
+
+    /**
+     * @param InventoryTransactionEvent $event
+     */
+    public function onInventoryTransaction(InventoryTransactionEvent $event): void {
+        $transaction = $event->getTransaction();
+        $player = null;
+        foreach ($transaction->getActions() as $action) {
+            $inventory = $action->getInventory();
+            if ($inventory instanceof AnvilInventory) {
+                $player = $transaction->getSource();
+                break;
+            }
+        }
+
+        if ($player instanceof Player && $inventory instanceof AnvilInventory) {
+            // Set the experience cost to 0
+            $inventory->setRepairCost(0);
+            $player->sendMessage("The anvil repair cost has been set to 0 XP!");
+        }
+    }
 }
