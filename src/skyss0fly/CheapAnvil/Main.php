f85149c2831299e2c2778c9a64249a566de1a83a
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-10-12 16:20:54 +1000

    Update Main.php

--- src/skyss0fly/CheapAnvil/Main.php
+++ src/skyss0fly/CheapAnvil/Main.php
@@ -7,38 +7,35 @@ use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\block\inventory\AnvilInventory;
 use pocketmine\player\Player;
 use pocketmine\inventory\transaction\action\SlotChangeAction;
-use pocketmine\item\enchantment\EnchantingOption;
-class Main extends PluginBase,EnchantingOption implements Listener{
-private $option;
 
+class Main extends PluginBase implements Listener {
+    
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->option = new EnchantingOption();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("CheapAnvil has been enabled and set Anvil Price to 0 xp");
+        $this->getLogger()->info("CheapAnvil has been enabled and set Anvil Price to 0 XP");
     }
 
     /**
+     * Handle inventory transactions and reduce XP cost for anvils.
+     *
      * @param InventoryTransactionEvent $event
      */
     public function onInventoryTransaction(InventoryTransactionEvent $event): void {
         $transaction = $event->getTransaction();
-        $player = null;
-        $inventory = null;
 
         foreach ($transaction->getActions() as $action) {
             if ($action instanceof SlotChangeAction) {
                 $inventory = $action->getInventory();
                 if ($inventory instanceof AnvilInventory) {
                     $player = $transaction->getSource();
-                    break;
+                    if ($player instanceof Player) {
+                        // Logic to set experience cost to 0.
+                        $inventory->setRepairCost(0);  // Example API call
+                        $this->getLogger()->info("XP cost set to 0 for player " . $player->getName());
+                        break;
+                    }
                 }
             }
-        }
-
-        if ($player instanceof Player && $inventory instanceof AnvilInventory) {
-            // Set the experience cost to 0
-            $this->option->$requiredXpLevel = 0;
         }
     }
 }
